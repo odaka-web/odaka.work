fa79dd2bc368ef3443676cac3ec618072c249dfc
odaka-yuki <54489400+odaka-yuki@users.noreply.github.com>
2023-12-12 00:59:49 +0900

    SocialLinkコンポーネント

--- app/components/SocialLink/index.js
+++ app/components/SocialLink/index.js
@@ -5,16 +5,19 @@ import logoX from "./logo-x.png";
 import styles from "./style.module.css";
 
 export default function SocialLink() {
+
+	const iconSize = 24;
+
 	return (
 		<ul className={styles.list}>
 			<li>
 				<Link href="https://github.com/odaka-yuki" target="_blank">
-					<Image src={logoGithub} alt="GitHub" width={24} height={24} title="Github @odaka-yuki" />
+					<Image src={logoGithub} alt="GitHub" width={iconSize} height={iconSize} title="Github @odaka-yuki" />
 				</Link>
 				</li>
 			<li>
 				<Link href="https://twitter.com/odaka_web" target="_blank">
-					<Image src={logoX} alt="X" width={24} height={24} title="X @odaka_web" />
+					<Image src={logoX} alt="X" width={iconSize} height={iconSize} title="X @odaka_web" />
 				</Link>
 			</li>
 		</ul>
