3947350d79689f76d6791e7906740d98a960a8a5
odaka-yuki <54489400+odaka-yuki@users.noreply.github.com>
2023-12-12 00:59:15 +0900

    レイアウト

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,27 @@
-export default function RootLayout({children}) {
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import "./style-base.css";
+import "./style-reset.css";
+import styles from "./style.module.css";
+
+export default function RootLayout({ children }) {
 	return (
 		<html lang="ja">
-			<body>
-				{children}
+			<body className={styles.body}>
+				<Header />
+				<main className={styles.main}>
+					{children}
+				</main>
+				<Footer />
 			</body>
 		</html>
 	);
 }
+
+export const metadata = {
+	title: {
+		default: `尾高 友紀（odaka yuki）`,
+		template: `%s | 尾高 友紀（odaka yuki）`,
+	},
+	description: ``,
+};
